bc1ee83637994fd5697f8252fb168e25d71d27bc
Peter Clifton <peter@clifton-electronics.co.uk>
2016-12-04 20:31:12 +0000

    Add "store_undo" parameter to some find.c functions.
    Tying the store of undo information and redrawing together is unhelpful!

--- src/select.c
+++ src/select.c
@@ -978,7 +978,7 @@ SelectObjectByName (int Type, char *Pattern, bool select)
   if (Type & NET_TYPE)
     {
       InitConnectionLookup ();
-      changed = ClearFlagOnAllObjects (true, FOUNDFLAG) || changed;
+      changed = ClearFlagOnAllObjects (true, FOUNDFLAG, true) || changed;
 
       MENU_LOOP (&PCB->NetlistLib);
       {
@@ -999,7 +999,7 @@ SelectObjectByName (int Type, char *Pattern, bool select)
       END_LOOP;
 
       changed = SelectByFlag (FOUNDFLAG, select) || changed;
-      changed = ClearFlagOnAllObjects (false, FOUNDFLAG) || changed;
+      changed = ClearFlagOnAllObjects (false, FOUNDFLAG, false) || changed;
       FreeConnectionLookupMemory ();
     }
 
