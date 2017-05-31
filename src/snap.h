f0779ad6f0af638438bce574999385908c0f16ab
Charles Parker <parker.charles@gmail.com>
2017-05-31 18:41:16 -0400

    Add lots of comments

--- src/snap.h
+++ src/snap.h
@@ -1,7 +1,7 @@
 /*!
  * \file src/snap.h
  *
- * \brief Snapping functions.
+ * \brief Snapping structures and functions.
  *
  * <hr>
  *
@@ -44,13 +44,26 @@
 struct SnapSpecStruct;
 typedef struct SnapSpecStruct SnapSpecType;
 
-/* should I include a pointer to the actual object? */
+/*!
+ * A structure that contains information about an object that is the result of a
+   snap search.
+ 
+   This should probably include a pointer to the object that was found. If it
+   did then there may be no need for the obj_type. The object would also know
+   where it is, but the snap point might not actually be where the object it, 
+   for example, if we're snapping to a point along a line.
+ */
 typedef struct
 {
+  /*! A pointer to the SnapSpec that this came from. */
   SnapSpecType * spec;
+  /*! A bool to indicate that an object was found. */
   bool valid;
+  /*! The point to snap to. */
   PointType loc;
+  /*! The square of the distance from the mouse pointer to the snap point. */
   Coord distsq;
+  /*! The type of the object that was found. */
   unsigned obj_type;
 } SnapType;
 
@@ -58,31 +71,83 @@ typedef struct
  * Snap Spec Type and functions
  */
 
+/*
+ * A SnapSpec is a structure that contains information about snapping to
+ * something. The search function is called to look for something.
+ */
 struct SnapSpecStruct
 {
+  /*! The name of the snap.*/
   char * name;
+  //! A function to search for objects to snap to
+  /*!
+   There is a question in my mind about what the call signature for this
+   function should look like. Should it take the radius like it currently does?
+   Or should it take a pointer to the snap spec? Presently the function has no
+   way to know anything about anything outside of itself.
+   \param x x coordinate to search
+   \param y y coordinate to search
+   \param r radius for the search
+   \return A SnapType object containing the result of the search
+   */
   SnapType (*search)(Coord x, Coord y, Coord r);
+  //! Enable flag
+  /*! A flag to indicate that the search should be executed or skipped. */
   bool enabled;
+  //! Snapping priority
+  /*! Objects are searched for in priority order. Things with a higher priority
+      are searched for first. */
   int priority;
+  //! Search radius
+  /*! The maximum distance away from the pointer that the search function should
+      look for objects. */
   Coord radius;
+  //! The type of object this spec searches for.
   int obj_type;
 };
 
 /* how many parameters ought to be specified in this constructor? 
    For insertion into a snap list it needs at least a name and a priority,
    so I'll go with that for now. */
-/* constructor */
+
+/*! \brief SnapSpec constructor 
+    \param name The name of the SnapSpec
+    \param priority The priority of the snapping.
+    \return The new SnapSpec structure.
+    \sa SnapSpecStruct
+ */
 SnapSpecType * snap_spec_new(char * name, int priority);
-/* destructor */
+/*! \brief SnapSpec destructor
+ *  \param snap The snap to free from memory.
+ */
 void snap_spec_delete(SnapSpecType * snap);
 
 /*
  * Snap List Type and functions
  */
 
+/*! \brief A structure containing an ordered list of SnapSpecs.
+ 
+    This structure implements a list of SnapSpecs. When an element is added, a
+    copy is made in the memory of the structure. This is done in order to keep
+    all of the objects to be iterated over in the same region of memory in order
+    to keep the iteration fast. The iteration must be fast since the list may be
+    iterated through every time the mouse is moved.
+ 
+    When SnapSpecs are added, they are inserted into the list at a position
+    according to their priority. The memory is always kept contiguous, so the
+    elements after are all shifted down in memory to make room for the new one.
+ 
+    If an element is subsequently removed, the size of the allocated memory is
+    not reduced, but the memory is still held. The elements in the list after 
+    the one removed are all shifted in memory so that all the elements in the
+    list remain contiguous. When the element is removed, \a n is decremented,
+    but max is not. Another snap can be added and fill that memory.
+ */
 typedef struct {
-  int n, max;
-  SnapSpecType * snaps;
+  int n,   /*! The number of elements in the list. */
+      max; /*! The maximum number of elements the list can presently hold. */
+  SnapSpecType * snaps; /*! The list of snaps. */
 } SnapListType;
 
 /* constructor */
