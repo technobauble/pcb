91edaaf3d26792b0e8235a050f39a4efb09fe236
Charles Parker <parker.charles@gmail.com>
2018-01-28 18:21:04 -0500

    use cursor_position_relative_format

--- src/hid/gtk/gui-misc.c
+++ src/hid/gtk/gui-misc.c
@@ -510,7 +510,7 @@ ghid_set_cursor_position_labels (void)
       Coord r  = Distance (Crosshair.X, Crosshair.Y, Marked.X, Marked.Y);
       double a = atan2 (dy, dx) * RAD_TO_DEG;
 
-      text = pcb_g_strdup_printf (_("%m+r %-mS;\nphi %-.1f;\n%-mS %-mS"),
+      text = pcb_g_strdup_printf (_(cursor_position_relative_format),
                                   Settings.grid_unit->allow,
                                   r, a, dx, dy);
       ghid_cursor_position_relative_label_set_text (text);
@@ -518,7 +518,7 @@ ghid_set_cursor_position_labels (void)
       no_relative_position = 0;
     }
   else
-    ghid_cursor_position_relative_label_set_text (_("r __.__;\nphi __._;\n__.__ __.__"));
+    ghid_cursor_position_relative_label_set_text (_("r __.__; phi __._; __.__ __.__"));
 
 
   text = pcb_g_strdup_printf (cursor_position_format,
